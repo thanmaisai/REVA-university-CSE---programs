fe2105cb85a52b7e6eea972f34a243c71962394a
THANMAI SAI <91474486+thanmaisai@users.noreply.github.com>
2022-02-16 11:02:49 +0530

    lambda

--- python/lambda_exp.py
+++ python/lambda_exp.py
@@ -1,5 +1,5 @@
 # Python program to print Even and odd Numbers in a List
-'''
+
 list1 = [10, 21, 4, 45, 66, 93, 11,2]
 #lambda expression 
 # filter--The filter function returns a sequence of those elements of iterable object for which function returns true value.
@@ -10,7 +10,7 @@ odd_nos = list(filter(lambda x: (x % 2 != 0), list1))
 
 print("Odd numbers in the list: ", odd_nos)
 print("Even numbers in the list: ", even_nos)
-'''
+
 
 
 
