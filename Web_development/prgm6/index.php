0f9cbbd7f407e9bfe827a7e711086342119f7915
THANMAI SAI <thanmaisayee123@gmail.com>
2022-11-23 10:21:24 +0000

    added 6.php

---
+++ Web_development/prgm6/index.php
@@ -0,0 +1,15 @@
+<?php
+$present_time=date("H:i:s-m/d/y");
+$expiry= 60 * 60 *24 *60 + time();
+setcookie("Lastvisit",$present_time, $expiry);
+if(isset($_COOKIE[“Lastvisit”]))
+{
+echo "Cookie has been set";
+echo "he current time of the system is";
+echo $present_time;
+echo "The Last visited Time and Date";
+echo $_COOKIE["Lastvisit"];
+}
+else
+echo "You’ve got some old cookies!";
+?>
