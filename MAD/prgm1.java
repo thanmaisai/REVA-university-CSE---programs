e54361015c053f3d5b7895b3e52553d061757f1a
thanmaisai <91474486+thanmaisai@users.noreply.github.com>
2023-03-27 21:25:27 +0530

    Create prgm1.java

---
+++ MAD/prgm1.java
@@ -0,0 +1,29 @@
+
+package com.example.lab1prgm;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent; import android.net.Uri;
+import android.os.Bundle; import android.view.View; import android.widget.Button;
+public class MainActivity extends AppCompatActivity 
+{ 
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); 
+        button=findViewById(R.id.button);
+        button.setOnClickListener
+        (
+            new View.OnClickListener() 
+            { 
+                @Override
+                public void onClick(View view) 
+                {
+                    Intent i=new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse("https://www.reva.edu.in"));
+                    startActivity(i);
+                }
+            }
+        ); 
+    }
+}
