eec7b55a9094ca83755733ef9d8ea0d0524a6160
THANMAI SAI <thanmaisayee123@gmail.com>
2023-06-08 11:39:37 +0530

    freq char in str

---
+++ Placement/Day3/freq_str.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class freq_str {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().toLowerCase();
+        int n = str.length();
+        Integer [] arr = new Integer [26];
+        for(int i=0;i<26;i++){
+            arr[i]=0;
+        }
+        for(int i=0;i<n;i++){
+            if((int)str.charAt(i)>=97 && (int)str.charAt(i)<=123){
+                arr[str.charAt(i)-97]++;
+            }
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]>0){
+                System.out.println((char)(i+97)+":"+arr[i]);
+            }
+        }
+    }
+    
+}
