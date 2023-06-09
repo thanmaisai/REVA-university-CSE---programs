0931722892ada54af0562c13b90ce158c09332fa
THANMAI SAI <thanmaisayee123@gmail.com>
2023-06-09 09:57:46 +0530

    shortened code for anagram

--- Placement/Day4/string_anagram.java
+++ Placement/Day4/string_anagram.java
@@ -8,21 +8,26 @@ public class string_anagram {
     Arrays.sort(arr1);
     char [] arr2 = (str2.toCharArray());
     Arrays.sort(arr2);
-    Boolean flag=false;
-    if(str1.length()!=str2.length()){
-        System.out.println("Not Anagram");
-    }else{
-        for(int i=0;i<arr1.length;i++){
-            if(arr2[i]==arr2[i]){
-                flag=true;
-            }
-        }
-        if(flag==true){
+    // Boolean flag=false;
+    // if(str1.length()!=str2.length()){
+    //     System.out.println("Not Anagram");
+    // }else{
+        if(Arrays.equals(arr1, arr2)){
             System.out.println("Anagram");
-        }else{
-            System.out.println("Not Anagram");
-
         }
+        else{
+            System.out.println("Not Anagram");
+        }
+        // for(int i=0;i<arr1.length;i++){
+        //     if(arr2[i]==arr2[i]){
+        //         flag=true;
+        //     }
+        // }
+        // if(flag==true){
+        //     System.out.println("Anagram");
+        // }else{
+        //     System.out.println("Not Anagram");
+
+        // }
     }
  }
-}
