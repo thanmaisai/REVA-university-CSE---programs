d5780e3dde90dc21dac6f348a324b1b9b055209b
THANMAI SAI <thanmaisayee123@gmail.com>
2023-07-03 23:53:49 +0530

    added factorial.java

--- Placement/Recurssion/factorial.java
+++ Placement/Recurssion/factorial.java
@@ -1,11 +1,11 @@
 public class factorial {
     public static void main(String[] args) {
-       System.out.println( factorial(5));
+       System.out.println( factorial_num(5));
     }
 
     //approach:- TC: O(n)
-    static int factorial(int n){
-        if(n==1) return 1;
-        return n * factorial(n-1);
+    static int factorial_num(int n){
+        if(n<=1) return 1;
+        return n * factorial_num(n-1);
     }
 }
